63bdba0b4aa73b83c69da331d437334c21d8ec20
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-12-02 18:44:17 +0100

    input manager class.

---
+++ input_manager.py
@@ -0,0 +1,24 @@
+import curses
+
+class InputManager():
+    def __init__(self, stdscr): 
+        self.stdscr = stdscr
+    
+
+    def get_input(self, prompt: str):
+        self.stdscr.clear()
+        self.stdscr.addstr(0, 0, prompt)
+        self.stdscr.refresh()
+        curses.echo()
+        input_str = self.stdscr.getstr(1, 0).decode('utf-8')
+        curses.noecho()
+        return input_str
+
+
+    def get_product_input(self,prefix: str):
+        name = self.get_input(f"{prefix} name: ")
+        desc = self.get_input(f"{prefix} description: ")
+        price = self.get_input(f"{prefix} price: ")
+        quantity = self.get_input(f"{prefix} quantity: ")
+
+        return (name, desc, price, quantity)
